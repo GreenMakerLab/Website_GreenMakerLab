8f75d076deea407d7dd4f030c5922215345c5222
Jefferson <jeffersonteodoro219@gmail.com>
2024-11-16 08:31:44 -0300

    Correção e atualização - TEAM

--- GreenMakerLab/src/components/Teamlist.jsx
+++ GreenMakerLab/src/components/Teamlist.jsx
@@ -1,31 +1,33 @@
 
 export const TeamList = [
-        {name: "Carlos", post: "Leader", description: "Et deleniti internos ut delectus dicta vel quisquam neque eos doloremque adipisci sed nihil voluptas et galisum assumenda. Sit voluptas minus est vitae eaque ea voluptatem blanditiis ea quia voluptatem.", img:"/./img/Carlos.png", curriculum:"https://lattes.cnpq.br/8552305760747009"},
+        {       
+                name: "Carlos Alberto da Silva Júnior ", post: "Leader", description: "Exerce o cargo de professor efetivo de química do Ensino Básico, Técnico e Tecnológico (EBTT) no Instituto Federal de Educação, Ciência e Tecnologia da Paraíba (IFPB) - Campus Sousa, como servidor público federal, desde janeiro de 2021. Doutor em Ciências pela Universidade Estadual de Campinas (UNICAMP), mestre e licenciado em Química. Tem experiência na área de Química, com ênfase em Química Analítica, atuando principalmente nos seguintes temas: experimentação e MALDI-Q TOF MS. Tem experiência de estágio no Departamento de Química Analítica e Ambiental da Universidade de Sherbrooke, Canadá (2015), onde desenvolveu projetos nessa área, pelo Programa Ciências sem Fronteiras (2014-2015). Tem atuado no Ensino de Química Verde e por seus trabalhos nesse tema foi premiado sete vezes no país e exterior (Chile e Itália).", img:"/./img/Carlos.jpeg", curriculum:"https://lattes.cnpq.br/8552305760747009"},
         {
-        name:  "Afonso Serafim Jacinto ", post: "Collide", description: "Possui graduação em Computação pela Universidade Estadual da Paraíba (2018) e mestrado em Tecnologia da Informação pelo Instituto Federal de Educação, Ciência e Tecnologia da Paraíba (2024). Atualmente é professor do Instituto Federal da Paraíba - IFPB atuando na área de Informática, com ênfase em Desenvolvimento de Software, Jogos Digitais e Educação em Computação.", img:"/./img/Afonso.png", curriculum:"http://lattes.cnpq.br/1535037984412459"
+                name:  "Afonso Serafim Jacinto ", post: "Collide", description: "Possui graduação em Computação pela Universidade Estadual da Paraíba (2018) e mestrado em Tecnologia da Informação pelo Instituto Federal de Educação, Ciência e Tecnologia da Paraíba (2024). Atualmente é professor do Instituto Federal da Paraíba - IFPB atuando na área de Informática, com ênfase em Desenvolvimento de Software, Jogos Digitais e Educação em Computação.", img:"/./img/Afonso.png", curriculum:"http://lattes.cnpq.br/1535037984412459"
         },
         {
-         name: "André Alexandre Honorio da Silveira", post: "Member", description:"Graduando em Licenciatura em Química pelo IFPB- Campus Sousa, membro do grupo de pesquisa Green Maker Lab deste mesmo campus, atuou no Programa Institucional de Bolsa de Iniciação a Docência no período 2023-2024. Formado no curso técnico em Agroindústria pelo IFPB- Campus Sousa, tendo estágio com ênfase na área de Leite e derivados, e análises Físico-Químicas com conhecimento de Informática básica.", img: "/./img/Andre.png", curriculum:" http://lattes.cnpq.br/6886352419255408"
+                name: "André Alexandre Honorio da Silveira", post: "Member", description:"Graduando em Licenciatura em Química pelo IFPB- Campus Sousa, membro do grupo de pesquisa Green Maker Lab deste mesmo campus, atuou no Programa Institucional de Bolsa de Iniciação a Docência no período 2023-2024. Formado no curso técnico em Agroindústria pelo IFPB- Campus Sousa, tendo estágio com ênfase na área de Leite e derivados, e análises Físico-Químicas com conhecimento de Informática básica.", img: "/./img/Andre.png", curriculum:" http://lattes.cnpq.br/6886352419255408"
         }, 
         {
-         name: "Daiane Dantas da Silva", post: "Member", description: "Possui ensino-medio-segundo-graupela José de Paiva Gadelha(2018).", img:"/./img/Daiane.png", curriculum:"https://lattes.cnpq.br/6831782165818587",
+                name: "Daiane Dantas da Silva", post: "Member", description: "Possui ensino-medio-segundo-graupela José de Paiva Gadelha(2018).", img:"/./img/Daiane.png", curriculum:"https://lattes.cnpq.br/6831782165818587",
         },
         {
                 name: "Denilson Santos Alves", post: "Member", description: "Possui ensino-fundamental-primeiro-graupela E.M.E.I.E.F Maria Vieira de Sousa(2022).",
                 img:"/./img/Denilson.png", curriculum: "https://lattes.cnpq.br/7932715152047559",
         },
         {
-                name: "Jefferson da Rocha Teodoro", post:"Member", description: "Atualmente é jovem aprendiz - Complexo hospitalar santa Terezinha e aluno do 3 ano de informática do IFPB-Campus Sousa. Possui Formações em cursos online de programação como também projetos pessoais. Participa/participou de projetos de extensão e pesquisa.", img:"/./img/Jefferson.png", curriculum: "http://lattes.cnpq.br/3099256114992204"
+                name: "Jefferson da Rocha Teodoro", post:"Member", description: "Foi Jovem Aprendiz no Complexo Hospitalar Santa Terezinha e atualmente é estudante do 3º ano do curso técnico em Informática integrado ao Ensino Médio no IFPB - Campus Sousa. Possui formações em cursos online na área de programação e experiência com projetos pessoais. Participa ou participou de projetos de extensão e pesquisa, além de ter participado de eventos acadêmicos oferecidos pelo campus, onde foi reconhecido com premiações.", img:"/./img/Jefferson.png", curriculum: "http://lattes.cnpq.br/3099256114992204"
         },
         {
                 name:"Jaelson Marques Martins", post: "Member", description: "Graduando em Química pelo Instituto Federal de Educação, Ciências e Tecnologia da Paraíba - IFPB, Campus Sousa-PBGraduado em Administração pela Universidade Federal de Campina Grande - UFCG, campus Sousa-PB. ", img:"/./img/Jaelson.png", curriculum:" http://lattes.cnpq.br/0057145569854119"
         },
         {
-                name: "Lucas de Sá Batista", post: "Member", description:"Possui ensino-fundamental-primeiro-graupela EEEFM PROFESSORA ANTONIA RANGEL DE FARIAS(2010) e ensino-medio-segundo-graupelo Instituto Federal de Educação, Ciência e Tecnologia da Paraíba - IFPB(2015). Tem experiência na área de Química.", img: "", curriculum: "http://lattes.cnpq.br/6771748788291885"
+                name: "Lucas de Sá Batista", post: "Member", description:"Possui ensino-fundamental-primeiro-graupela EEEFM PROFESSORA ANTONIA RANGEL DE FARIAS(2010) e ensino-medio-segundo-graupelo Instituto Federal de Educação, Ciência e Tecnologia da Paraíba - IFPB(2015). Tem experiência na área de Química.", 
+                img:"/./img/Lucas.png" , curriculum: "http://lattes.cnpq.br/6771748788291885"
         },
         {
-                name: "Francisco Tavares Pedrosa Júnior ", post: "Member", description: "Possui ensino-fundamental-primeiro-graupela Escola Municipal de Ensino Fundamental Maria do Carmo Pedroza Mendes(2020) e ensino-medio-segundo-graupelo Instituto Federal de Educação, Ciência e Tecnologia da Paraíba - IFPB(2023).", img: "", curriculum: "http://lattes.cnpq.br/7030009945500460", 
-        }, 
+                name: "Francisco Tavares Pedrosa Júnior ", post: "Member", description: "Possui ensino-fundamental-primeiro-graupela Escola Municipal de Ensino Fundamental Maria do Carmo Pedroza Mendes(2020) e ensino-medio-segundo-graupelo Instituto Federal de Educação, Ciência e Tecnologia da Paraíba - IFPB(2023).", img: "/./img/Tavares.PNG", curriculum: "http://lattes.cnpq.br/7030009945500460", 
+        },
         {
                 name: "José Guilherme Gomes Queiroz", post: "Member", description: "Graduando em Licenciatura em Química pelo Instituto Federal de Educação ciência e tecnologia Paraíba-IFPB.", img:"/./img/Guilherme.png", curriculum:"http://lattes.cnpq.br/1130339911285532"
         },
